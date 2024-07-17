04bc08a537002d7c50a1b1c12ade7b9fe8b83a94
Ifrah Naaz <120669161+ifrahnz26@users.noreply.github.com>
2024-07-17 16:02:00 +0530

    Update 3 CODE: quickSort.py

--- 3 CODE: quickSort.py
+++ 3 CODE: quickSort.py
@@ -11,19 +11,19 @@ import time
 def partition(A, l, r):
     p = A[l]  # Selecting the first element as pivot
     i = l     # Left pointer starting at the beginning of the array
-    j = r+1     # Right pointer starting at the end of the array
+    j = r + 1 # Right pointer starting just after the pivot
     
     while i < j:
-        while A[i] < p:
+        while i <= r and A[i] <= p:
             i += 1
         while A[j] > p:
             j -= 1
         if i < j:
             A[i], A[j] = A[j], A[i]
     
-    # Swap pivot with A[j] to put the pivot in its correct place
-    A[l], A[j] = A[j], A[l]
-    return j
+    # Swap pivot with A[j-1] to put the pivot in its correct place
+    A[l], A[j-1] = A[j-1], A[l]
+    return j-1
 
 def quicksort(A, l, r):
     if l < r:
@@ -31,17 +31,18 @@ def quicksort(A, l, r):
         s = partition(A, l, r)
         
         # Recursively sort the left and right parts
-        quicksort(A, l, s)     # Sort the left part
-        quicksort(A, s + 1, r) # Sort the right part
+        quicksort(A, l, s-1)     # Sort the left part
+        quicksort(A, s + 1, r)   # Sort the right part
 
 # Input array from user
 A = list(map(int, input("Enter the elements: ").split()))
+n = len(A)
 
 # Measure the start time of the sorting process
 start = time.time()
 
 # Sort the array using quicksort algorithm
-quicksort(A, 0, len(A) - 2)
+quicksort(A, 0, n - 1)
 
 # Measure the end time of the sorting process
 end = time.time()
